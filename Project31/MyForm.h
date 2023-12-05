62b77e5796d76809b25cb2dae4dc76514bd8e74d
mltk8 <114109499+mltk8@users.noreply.github.com>
2023-12-05 16:45:44 +0200

    /zrada_of_pes_patron/

--- Project31/MyForm.h
+++ Project31/MyForm.h
@@ -16,7 +16,7 @@ const int PORT = 12345;
 
 
 namespace Project31 {
-
+#define PACKET_BUFF_SIZE 10
 	using namespace System;
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
@@ -34,10 +34,10 @@ namespace Project31 {
 		bool wf = false;
 		bool sp = false;
 		bool er = false;
-	private: System::Windows::Forms::PictureBox^ pictureBox1;
 
-	private: System::Windows::Forms::PictureBox^ pictureBox3;
-	private: System::Windows::Forms::PictureBox^ pictureBox2;
+
+
+
 
 	public:
 		bool isListening = false;
@@ -45,37 +45,61 @@ namespace Project31 {
 	public:
 		MyForm(void)
 		{
+
 			InitializeComponent();
-			sockaddr_in serverAddress{};
-			// Use inet_pton to convert an IP address from a string representation
-			if (inet_pton(AF_INET, "127.0.0.1", &(serverAddress.sin_addr)) != 1) {
-				cerr << "Invalid IP address" << endl;
-				WSACleanup();
-				return;
+			try {
+				
+				
+				
+				WSADATA wsaData;
+				if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+				cerr << "Failed to initialize Winsock!" << endl;
+				String^ err = "Failed to initialize Winsock!";
+			    throw err;
+				}
+
+				sockaddr_in serverAddress{};
+				// Use inet_pton to convert an IP address from a string representation
+				if (inet_pton(AF_INET, "127.0.0.1", &(serverAddress.sin_addr)) != 1) {
+					cerr << "Invalid IP address" << endl;
+					WSACleanup();
+					String^ err = "Invalid IP address";
+					throw err;
+
+				}
+				serverAddress.sin_port = htons(PORT);
+				serverAddress.sin_family = AF_INET;
+
+				// Creating server socket
+				// AF_INET - internet socket
+				// SOCK_STREAM - stream socket (with creating a connection)
+				// NULL - default TCP protocol
+
+
+
+				clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+				if (clientSocket == INVALID_SOCKET) {
+					cerr << "Error creating socket" << endl;
+					WSACleanup();
+					String^ err = "Error creating socket";
+					throw err;
+				}
+
+				if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) != 0) {
+					cerr << "Connection failed" << std::endl;
+					closesocket(clientSocket);
+					WSACleanup();
+					String^ err = "Connection failed";
+					throw err;
+				}
 			}
-			serverAddress.sin_port = htons(PORT);
-			serverAddress.sin_family = AF_INET;
-
-			// Creating server socket
-			// AF_INET - internet socket
-			// SOCK_STREAM - stream socket (with creating a connection)
-			// NULL - default TCP protocol
-		
-
-
-			clientSocket = socket(AF_INET, SOCK_STREAM, NULL);
-			if (clientSocket == INVALID_SOCKET) {
-				cerr << "Error creating socket" << endl;
-				WSACleanup();
-				return;
+			catch (String^ ex) {
+			
+				MessageBox::Show(ex, "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+				exit(-1);
 			}
-
-			if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) != 0) {
-				cerr << "Connection failed" << std::endl;
-				closesocket(clientSocket);
-				WSACleanup();
-				return;
-			}
+				
+			
 
 
 			
@@ -105,7 +129,9 @@ namespace Project31 {
 				tb_er->Text = gcnew String(str.c_str());
 			}
 			else {
-				throw -1;
+				delete[] buffer;
+				MessageBox::Show("Received Invalid Data From Serever", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+				exit(-1);
 			}
 			memset(buffer, '\0', 1000);
 
@@ -207,7 +233,7 @@ namespace Project31 {
 			}
 		}
 
-	private: System::Windows::Forms::Label^ label1;
+
 	protected:
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::Label^ label3;
@@ -233,7 +259,6 @@ namespace Project31 {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
-			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
@@ -243,96 +268,82 @@ namespace Project31 {
 			this->btn_wf = (gcnew System::Windows::Forms::Button());
 			this->btn_sp = (gcnew System::Windows::Forms::Button());
 			this->btn_er = (gcnew System::Windows::Forms::Button());
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
-			this->pictureBox3 = (gcnew System::Windows::Forms::PictureBox());
-			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->BeginInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
 			this->SuspendLayout();
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Perpetua Titling MT", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(386, 30);
-			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(248, 36);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"Fomo Sniffer";
-			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label2->Location = System::Drawing::Point(73, 108);
+			this->label2->BackColor = System::Drawing::Color::SteelBlue;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft JhengHei UI", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::Color::Black;
+			this->label2->Location = System::Drawing::Point(29, 260);
 			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(212, 25);
+			this->label2->Size = System::Drawing::Size(261, 29);
 			this->label2->TabIndex = 1;
-			this->label2->Text = L"Weather forecast(60 s)";
+			this->label2->Text = L"Weather forecast (60 s)";
+			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label3->Location = System::Drawing::Point(425, 397);
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft JhengHei UI", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(29, 34);
 			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(152, 25);
+			this->label3->Size = System::Drawing::Size(184, 29);
 			this->label3->TabIndex = 2;
-			this->label3->Text = L"Share price(1 s)";
+			this->label3->Text = L"Share price (1 s)";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label4->Location = System::Drawing::Point(713, 108);
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft JhengHei UI", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->Location = System::Drawing::Point(29, 492);
 			this->label4->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(221, 25);
+			this->label4->Size = System::Drawing::Size(264, 29);
 			this->label4->TabIndex = 3;
-			this->label4->Text = L"Exchange rates(1440 s)";
+			this->label4->Text = L"Exchange rates (1440 s)";
 			// 
 			// tb_wf
 			// 
-			this->tb_wf->Location = System::Drawing::Point(32, 148);
+			this->tb_wf->Location = System::Drawing::Point(31, 293);
 			this->tb_wf->Margin = System::Windows::Forms::Padding(4);
 			this->tb_wf->Name = L"tb_wf";
-			this->tb_wf->Size = System::Drawing::Size(300, 127);
+			this->tb_wf->Size = System::Drawing::Size(363, 127);
 			this->tb_wf->TabIndex = 4;
 			this->tb_wf->Text = L"";
 			// 
 			// tb_sp
 			// 
-			this->tb_sp->Location = System::Drawing::Point(359, 437);
+			this->tb_sp->Location = System::Drawing::Point(28, 67);
 			this->tb_sp->Margin = System::Windows::Forms::Padding(4);
 			this->tb_sp->Name = L"tb_sp";
-			this->tb_sp->Size = System::Drawing::Size(300, 127);
+			this->tb_sp->Size = System::Drawing::Size(364, 127);
 			this->tb_sp->TabIndex = 5;
 			this->tb_sp->Text = L"";
 			// 
 			// tb_er
 			// 
-			this->tb_er->Location = System::Drawing::Point(684, 148);
+			this->tb_er->Location = System::Drawing::Point(32, 525);
 			this->tb_er->Margin = System::Windows::Forms::Padding(4);
 			this->tb_er->Name = L"tb_er";
-			this->tb_er->Size = System::Drawing::Size(300, 127);
+			this->tb_er->Size = System::Drawing::Size(363, 127);
 			this->tb_er->TabIndex = 6;
 			this->tb_er->Text = L"";
 			// 
 			// btn_wf
 			// 
-			this->btn_wf->Location = System::Drawing::Point(32, 318);
+			this->btn_wf->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->btn_wf->Location = System::Drawing::Point(443, 325);
 			this->btn_wf->Margin = System::Windows::Forms::Padding(4);
 			this->btn_wf->Name = L"btn_wf";
-			this->btn_wf->Size = System::Drawing::Size(301, 58);
+			this->btn_wf->Size = System::Drawing::Size(228, 58);
 			this->btn_wf->TabIndex = 7;
 			this->btn_wf->Text = L"Subscribe";
 			this->btn_wf->UseVisualStyleBackColor = true;
@@ -340,10 +351,12 @@ namespace Project31 {
 			// 
 			// btn_sp
 			// 
-			this->btn_sp->Location = System::Drawing::Point(359, 607);
+			this->btn_sp->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->btn_sp->Location = System::Drawing::Point(441, 102);
 			this->btn_sp->Margin = System::Windows::Forms::Padding(4);
 			this->btn_sp->Name = L"btn_sp";
-			this->btn_sp->Size = System::Drawing::Size(301, 58);
+			this->btn_sp->Size = System::Drawing::Size(228, 58);
 			this->btn_sp->TabIndex = 8;
 			this->btn_sp->Text = L"Subscribe";
 			this->btn_sp->UseVisualStyleBackColor = true;
@@ -351,60 +364,23 @@ namespace Project31 {
 			// 
 			// btn_er
 			// 
-			this->btn_er->Location = System::Drawing::Point(684, 318);
+			this->btn_er->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->btn_er->Location = System::Drawing::Point(444, 558);
 			this->btn_er->Margin = System::Windows::Forms::Padding(4);
 			this->btn_er->Name = L"btn_er";
-			this->btn_er->Size = System::Drawing::Size(301, 58);
+			this->btn_er->Size = System::Drawing::Size(228, 58);
 			this->btn_er->TabIndex = 9;
 			this->btn_er->Text = L"Subscribe";
 			this->btn_er->UseVisualStyleBackColor = true;
 			this->btn_er->Click += gcnew System::EventHandler(this, &MyForm::btn_er_Click);
 			// 
-			// pictureBox1
-			// 
-			this->pictureBox1->ErrorImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.ErrorImage")));
-			this->pictureBox1->ImageLocation = L"patron_fomo_there.jpg";
-			this->pictureBox1->InitialImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.InitialImage")));
-			this->pictureBox1->Location = System::Drawing::Point(57, 397);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(275, 268);
-			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
-			this->pictureBox1->TabIndex = 10;
-			this->pictureBox1->TabStop = false;
-			// 
-			// pictureBox3
-			// 
-			this->pictureBox3->ErrorImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox3.ErrorImage")));
-			this->pictureBox3->ImageLocation = L"patron_fomo_found.jpg";
-			this->pictureBox3->InitialImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox3.InitialImage")));
-			this->pictureBox3->Location = System::Drawing::Point(372, 108);
-			this->pictureBox3->Name = L"pictureBox3";
-			this->pictureBox3->Size = System::Drawing::Size(275, 268);
-			this->pictureBox3->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
-			this->pictureBox3->TabIndex = 12;
-			this->pictureBox3->TabStop = false;
-			// 
-			// pictureBox2
-			// 
-			this->pictureBox2->ErrorImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.ErrorImage")));
-			this->pictureBox2->ImageLocation = L"stiker-sobaka-patron-na-raketi_f.jpg";
-			this->pictureBox2->InitialImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.InitialImage")));
-			this->pictureBox2->Location = System::Drawing::Point(684, 397);
-			this->pictureBox2->Name = L"pictureBox2";
-			this->pictureBox2->Size = System::Drawing::Size(275, 268);
-			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
-			this->pictureBox2->TabIndex = 13;
-			this->pictureBox2->TabStop = false;
-			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::Color::Red;
-			this->ClientSize = System::Drawing::Size(1024, 707);
-			this->Controls->Add(this->pictureBox2);
-			this->Controls->Add(this->pictureBox3);
-			this->Controls->Add(this->pictureBox1);
+			this->BackColor = System::Drawing::Color::SteelBlue;
+			this->ClientSize = System::Drawing::Size(710, 711);
 			this->Controls->Add(this->btn_er);
 			this->Controls->Add(this->btn_sp);
 			this->Controls->Add(this->btn_wf);
@@ -414,19 +390,20 @@ namespace Project31 {
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
-			this->Controls->Add(this->label1);
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"MyForm";
-			this->Text = L"FOMO Sniffer";
+			this->Text = L"Mailing";
 			this->FormClosing += gcnew System::Windows::Forms::FormClosingEventHandler(this, &MyForm::MyForm_FormClosing);
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->EndInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+
+
+
+private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
